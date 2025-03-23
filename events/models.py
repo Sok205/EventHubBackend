5610fbdb21bab2421d11026575dd06fd654f1a04
sok205 <Filip007lol@gmail.com>
2025-03-23 10:12:22 +0100

    User login and registration

--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    bio = models.TextField(max_length=500, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    website = models.URLField(max_length=200, null=True, blank=True)
